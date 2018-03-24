a67548262512873926db95989fac909436a3facb
Balraj <balraj0496@gmail.com>
2018-03-24 20:30:19 +0530

    Add Divider decoration to steps recycler view and more hardcoded string keys to variables

--- app/src/main/java/balraj/se/bakingapp/UI/RecipeDetailActivity.java
+++ app/src/main/java/balraj/se/bakingapp/UI/RecipeDetailActivity.java
@@ -19,6 +19,8 @@ import balraj.se.bakingapp.Model.Ingredient;
 import balraj.se.bakingapp.Model.Recipe;
 import balraj.se.bakingapp.Model.Step;
 import balraj.se.bakingapp.R;
+import balraj.se.bakingapp.RecipeMainActivity;
+import balraj.se.bakingapp.Utils.DividerItemDecoration;
 import balraj.se.bakingapp.Widget.BakingUpdateService;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,9 +29,18 @@ import butterknife.ButterKnife;
  * Created by balra on 13-03-2018.
  */
 
-public class RecipeDetailActivity extends AppCompatActivity implements RecipeStepsAdapter.OnStepClickListener {
+public class RecipeDetailActivity extends AppCompatActivity implements RecipeStepsAdapter.OnStepClickListener,
+        RecipeStepDetailFragment.OnStepDetailItemClick {
 
+    //keys
     public static final String ING_LIST_KEY = "ing_list";
+    private static final String ING_FRAG_TAG = "ing_frag_tag";
+    private static final String SAVED_RECIPE_KEY = "saved_recipe_key";
+    private static final String SAVED_STEP_LIST_STATE_KEY = "list_state";
+    public static List<Step> steps = new ArrayList<>();
+    public static int stepSize;
+
+    //inject views
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.recipe_detail_rv)
@@ -52,8 +63,8 @@ public class RecipeDetailActivity extends AppCompatActivity implements RecipeSte
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
         Intent intent = getIntent();
-        List<Step> steps = new ArrayList<>();
 
+        //set back button in action bar / toolbar
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -61,25 +72,35 @@ public class RecipeDetailActivity extends AppCompatActivity implements RecipeSte
             actionBar.setTitle("");
         }
 
-        if (intent.hasExtra("cr")) {
-            mRecipe = intent.getParcelableExtra("cr");
-        } else if (savedInstanceState != null) {
-            mRecipe = savedInstanceState.getParcelable("recipe");
+        //check for recipe from calling activity
+        if (intent.hasExtra(RecipeMainActivity.RECIPE_KEY)) {
+            mRecipe = intent.getParcelableExtra(RecipeMainActivity.RECIPE_KEY);
+        }
+        //check for recipe on orientation change
+        else if (savedInstanceState != null) {
+            mRecipe = savedInstanceState.getParcelable(SAVED_RECIPE_KEY);
         }
 
+        //set title in action bar
         if (mRecipe != null) {
             steps = mRecipe.getSteps();
             if (actionBar != null)
                 actionBar.setTitle(mRecipe.getName());
         }
 
+        //initialise adapter for holding steps for recipe
         stepsAdapter = new RecipeStepsAdapter(this, steps,
                 this);
+
+        stepSize = steps.size();
         setUpRecyclerView();
+
+        //load layout manager state for recycler view
         if (savedInstanceState != null) {
-            listState = savedInstanceState.getParcelable("list_state");
+            listState = savedInstanceState.getParcelable(SAVED_STEP_LIST_STATE_KEY);
             stepsRecyclerView.getLayoutManager().onRestoreInstanceState(listState);
         }
+        //check for master detail layout
         if (findViewById(R.id.recipe_detail_container) != null) {
             // The detail container view will be present only in the
             // large-screen layouts (res/values-w900dp).
@@ -93,39 +114,57 @@ public class RecipeDetailActivity extends AppCompatActivity implements RecipeSte
 
     }
 
+    //helper function to setup recycler view
     private void setUpRecyclerView() {
         stepsLayoutManager = new LinearLayoutManager(this);
         stepsRecyclerView.setHasFixedSize(true);
         stepsRecyclerView.setAdapter(stepsAdapter);
         stepsRecyclerView.setLayoutManager(stepsLayoutManager);
+        stepsRecyclerView.addItemDecoration(
+                new DividerItemDecoration(this, R.drawable.divider,
+                        false, false));
     }
 
+    //save layout manager state in onPause
     @Override
     public void onPause() {
         super.onPause();
         listState = stepsRecyclerView.getLayoutManager().onSaveInstanceState();
     }
 
+    //persist data on orientation change
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putParcelable("list_state", listState);
-        outState.putParcelable("recipe", mRecipe);
+        outState.putParcelable(SAVED_STEP_LIST_STATE_KEY, listState);
+        outState.putParcelable(SAVED_RECIPE_KEY, mRecipe);
     }
 
+    //open step details on click some step
     @Override
-    public void onStepClick(Step clickedStep) {
+    public void onStepClick(Step clickedStep, int position) {
+        openStepDetails(clickedStep, position);
+    }
+
+    //helper function to open step details
+    public void openStepDetails(Step clickedStep, int position) {
         if (mTwoPane) {
             Bundle arguments = new Bundle();
-            arguments.putParcelable(RecipeDetailFragment.ARG_ITEM_ID, clickedStep);
-            RecipeDetailFragment fragment = new RecipeDetailFragment();
+            arguments.putParcelable(RecipeStepDetailFragment.ARG_ITEM, clickedStep);
+            arguments.putInt(RecipeStepDetailFragment.STEP_SIZE_KEY, steps.size());
+            arguments.putInt(RecipeStepDetailFragment.STEP_INDEX_KEY, position);
+            arguments.putBoolean(RecipeStepDetailFragment.TWO_PANE_KEY, mTwoPane);
+            RecipeStepDetailFragment fragment = new RecipeStepDetailFragment();
             fragment.setArguments(arguments);
             this.getSupportFragmentManager().beginTransaction()
                     .replace(R.id.recipe_detail_container, fragment)
                     .commit();
         } else {
             Intent intent = new Intent(this, RecipeStepDetailActivity.class);
-            intent.putExtra(RecipeDetailFragment.ARG_ITEM_ID, clickedStep);
+            intent.putExtra(RecipeStepDetailFragment.ARG_ITEM, clickedStep);
+            intent.putExtra(RecipeStepDetailFragment.STEP_SIZE_KEY, steps.size());
+            intent.putExtra(RecipeStepDetailFragment.STEP_INDEX_KEY, position);
+            intent.putExtra(RecipeStepDetailFragment.TWO_PANE_KEY, mTwoPane);
             this.startActivity(intent);
         }
     }
@@ -146,11 +185,19 @@ public class RecipeDetailActivity extends AppCompatActivity implements RecipeSte
         return true;
     }
 
+    //show ingredients dialog
     public void showIngredients(View view) {
         ingredientsFragment = new IngredientsFragment();
         Bundle args = new Bundle();
         args.putParcelableArrayList(ING_LIST_KEY, (ArrayList<Ingredient>) mRecipe.getIngredients());
         ingredientsFragment.setArguments(args);
-        ingredientsFragment.show(getFragmentManager(), "ing_tag");
+        ingredientsFragment.show(getFragmentManager(), ING_FRAG_TAG);
     }
+
+    //handle previous and next buttons click
+    @Override
+    public void onNextPrevClick(int position) {
+        openStepDetails(steps.get(position), position);
+    }
+
 }
